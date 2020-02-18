adcb47d78c072050187a0a5b6e1ae64f09316b97
Elliot Balynn <elliot@balynn.com>
2020-02-18 14:00:55 +0000

    Initial commit

---
+++ ConcreteViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Balynn.ViewModelDemo
+{
+    public class ConcreteViewModel : ViewModel
+    {
+        public string Name
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public int Age
+        {
+            get => Get<int>();
+            set => Set(value);
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Balynn.ViewModelDemo
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        private readonly Lazy<ConcurrentDictionary<string, object>> _propertyRefValues =
+            new Lazy<ConcurrentDictionary<string, object>>(LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private readonly Lazy<ConcurrentDictionary<string, PropertyChangedEventArgs>> _propertyChangedEventArgs =
+            new Lazy<ConcurrentDictionary<string, PropertyChangedEventArgs>>();
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual T Get<T>(T defaultValue = default, [CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException(nameof(propertyName));
+            }
+
+            if (_propertyRefValues.Value.ContainsKey(propertyName))
+            {
+                return (T)_propertyRefValues.Value[propertyName];
+            }
+
+            return defaultValue;
+        }
+
+        protected virtual bool Set<T>(T value, [CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException(nameof(propertyName));
+            }
+
+            var existing = Get(default(T), propertyName);
+
+            if (object.Equals(existing, value) == false)
+            {
+                _propertyRefValues.Value[propertyName] = value;
+
+                RaisePropertyChanged(propertyName);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException(nameof(propertyName));
+            }
+
+            var propertyArgs = _propertyChangedEventArgs.Value
+                .GetOrAdd(propertyName, new PropertyChangedEventArgs(propertyName));
+
+            PropertyChanged?.Invoke(this, propertyArgs);
+        }
+    }
+}
